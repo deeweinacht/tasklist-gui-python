26cf7c8fb0f5d4213c0ab3b6c06133fd675e2369
Dee <dweinacht@workmail.com>
2023-02-05 14:16:58 -0400

    Added web app functionality

--- web_interface.py
+++ web_interface.py
@@ -1,11 +1,28 @@
 import streamlit as st
 import functions
 
+task_list = functions.get_tasks()
+
+
+def add_task():
+    new_task = st.session_state['task_input']
+    task_list.append(new_task + '\n')
+    functions.save_tasks(task_list)
+
+
 st.title('Simple Task List')
 st.subheader('Use this page to track your daily tasks')
-
 st.write('Your tasks:')
-for task in functions.get_tasks():
-    st.checkbox(task)
 
-st.text_input(label= '', placeholder='Enter a new task...')
+for index, task in enumerate(task_list):
+    print(task)
+    checkbox = st.checkbox(task, key=task)
+    if checkbox:
+        task_list.pop(index)
+        functions.save_tasks(task_list)
+        del st.session_state[task]
+        st.experimental_rerun()
+
+st.text_input(label='text entry', label_visibility='hidden',
+              placeholder='Enter a new task...', on_change=add_task,
+              key='task_input')
